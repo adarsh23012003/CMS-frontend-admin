bff41144e8f2b2d482e6b67f1b83306936f10d60
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-08-11 12:54:02 +0530

    Contact, Home, NoPage created

---
+++ src/Pages/NoPage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NoPage() {
+  return (
+    <>
+      <div>
+        <h1 className='text-red-500 text-5xl'>404 Page Not Found</h1>
+      </div>
+    </>
+  );
+}
+
+export default NoPage;
