a8bfda3a3765ca95a8a50b503eb897070e716e71
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-08-19 15:27:34 +0530

    Navbar changes

--- src/Components/App/Navbar.js
+++ src/Components/App/Navbar.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { deleteCookie, getCookieComponent } from "../../Axios/cookieConfig";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const [isLogin, setIsLogin] = useState(false);
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate("/login");
+  };
+  useEffect(() => {
+    const token = getCookieComponent("token");
+    if (token) {
+      setIsLogin(true);
+    }
+  }, []);
+  const handleLogout = () => {
+    console.log("token delete");
+    const deleteToken = deleteCookie("token");
+    if (deleteToken) {
+      setIsLogin(false);
+      window.location.reload();
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <nav className='p-3 bg-slate-200'>
+          <ul className='flex justify-center items-center gap-5 font-semibold'>
+            <li
+              className='cursor-pointer'
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </li>
+            <li
+              className='cursor-pointer'
+              onClick={() => {
+                navigate("/about");
+              }}
+            >
+              About
+            </li>
+            <li
+              className='cursor-pointer'
+              onClick={() => {
+                navigate("/contact");
+              }}
+            >
+              Contact
+            </li>
+            <li
+              onClick={() => {
+                if (isLogin) {
+                  handleLogout();
+                } else {
+                  handleLogin();
+                }
+              }}
+              className='px-3 py-1.5 rounded-md bg-green-300 hover:bg-green-400 hover:text-white cursor-pointer'
+            >
+              {isLogin ? "Logout" : "Login"}
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
